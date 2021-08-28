ac2f59c397d9eeb72e777d2b211050f83b5702bf
Jon Garrity <jonnygarrity@gmail.com>
2021-08-27 19:11:15 -0500

    Added webpack config file creation, css loader install, git and Prettier ignore files. Added ESLint and Prettier install, config, and package for interoperability.

--- new-js-project.sh
+++ new-js-project.sh
@@ -31,3 +31,49 @@ echo "<!DOCTYPE html>
         <script src=\"main.js\"></script>
     </body>
 </html>" > index.html
+# step 3: create webpack.config.js file at project root and install css loader
+cd ..
+echo "const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+        ],
+    },
+};" > webpack.config.js
+npm install --save-dev style-loader css-loader
+# step 4: create generic .gitignore file
+echo "node_modules/
+dist/*
+!dist/index.html" > .gitignore
+# step 5: install ESLint and Prettier, create config and ignore files), install package allowing them to work together
+npm install eslint --save-dev
+npx eslint --init
+npm install --save-dev --save-exact prettier
+echo "{
+    \"trailingComma\": \"es5\",
+    \"tabWidth\": 4,
+    \"semi\": true,
+    \"singleQuote\": true
+}" > .prettierrc.json
+echo "node_modules
+dist" > .prettierignore
+npm install --save-dev eslint-config-prettier
+# Then, add "prettier" to the "extends" array in your .eslintrc.* file. Make sure to put it last, so it gets the chance to override other configs.
+# Rebuild webpack (add "build": "webpack") to package.json scripts object
+
